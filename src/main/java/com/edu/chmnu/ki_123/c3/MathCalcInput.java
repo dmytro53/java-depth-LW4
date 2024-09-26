9f9bd5c77b5a8259be01e384807443e544cad2db
Dimas <dimas28182@gmail.com>
2024-09-26 14:26:25 +0300

    Init

--- src/main/java/com/edu/chmnu/ki_123/c3/MathCalcInput.java
+++ src/main/java/com/edu/chmnu/ki_123/c3/MathCalcInput.java
@@ -5,23 +5,43 @@ import static java.lang.Math.*;
 
 public class MathCalcInput {
     public static void main(String[] args) {
-        try(Scanner scanner = new Scanner(System.in)) {
+        try (Scanner scanner = new Scanner(System.in)) {
             double x, a, b, c;
 
             System.out.print("Enter the value of x: ");
-            x = scanner.nextDouble();
+            if (scanner.hasNextDouble()) {
+                x = scanner.nextDouble();
+            } else {
+                System.err.println("You must enter a valid double for x!");
+                return;
+            }
 
             System.out.print("Enter the value of a: ");
-            a = scanner.nextDouble();
+            if (scanner.hasNextDouble()) {
+                a = scanner.nextDouble();
+            } else {
+                System.err.println("You must enter a valid double for a!");
+                return;
+            }
 
             System.out.print("Enter the value of b: ");
-            b = scanner.nextDouble();
+            if (scanner.hasNextDouble()) {
+                b = scanner.nextDouble();
+            } else {
+                System.err.println("You must enter a valid double for b!");
+                return;
+            }
 
             System.out.print("Enter the value of c: ");
-            c = scanner.nextDouble();
+            if (scanner.hasNextDouble()) {
+                c = scanner.nextDouble();
+            } else {
+                System.err.println("You must enter a valid double for c!");
+                return;
+            }
 
-            double result = (exp(a * cos(x+2))) - ((exp(- sin(b * x)))/(c - cbrt(x)));
-
+            double result = (exp(a * cos(x + 2))) - ((exp(-sin(b * x))) / (c - cbrt(x)));
+            
             System.out.printf("Result: %.3f\n", result);
         }
     }
